d711032f12a0aed56e614e6f058bd30fefb23586
yipjie <dev@otdd.io>
2020-01-03 03:51:26 +0800

    update script

--- install/uninstall.sh
+++ install/uninstall.sh
@@ -1,4 +1,4 @@
-kubectl -n istio-system get configmap istio-sidecar-injector -o yaml | sed "s/otdd\/proxyv2:1.2.2.alpha.0/istio\/proxyv2:1.2.2/g"| kubectl apply -f -
+kubectl -n istio-system get configmap istio-sidecar-injector -o yaml | sed "s/otdd\/proxyv2:1.2.2-alpha.0/istio\/proxyv2:1.2.2/g"| kubectl apply -f -
 kubectl -n istio-system rollout restart deploy/istio-sidecar-injector
 for i in artifacts/crd*yaml; do kubectl delete -f $i; done
 kubectl delete -f otdd.yaml
