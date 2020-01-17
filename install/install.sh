bbaf71b9db1096bb82c03a8045dd51be2e61164e
yipjie <dev@otdd.io>
2020-01-17 10:45:14 +0800

    update

--- install/install.sh
+++ install/install.sh
@@ -1,4 +1,8 @@
+#change istio proxyv2 to include otdd redirect/recorder plugins.
 kubectl -n istio-system get configmap istio-sidecar-injector -o yaml | sed "s/istio\/proxyv2:1.2.2/otdd\/proxyv2:1.2.2-alpha.0/g"| kubectl apply -f -
 kubectl -n istio-system rollout restart deploy/istio-sidecar-injector
+#install otdd recorder crd and it's k8s controller
 for i in artifacts/crd*yaml; do kubectl apply -f $i; done
 kubectl apply -f otdd.yaml
+#install istio mixer otdd adapter
+for i in artifacts/istio-mixer*.yaml; do kubectl apply -f $i; done
