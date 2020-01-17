28fb078b61ff2bf4da6e2e9e48329599a9f688c4
yipjie <dev@otdd.io>
2020-01-17 18:05:55 +0800

    update

--- install/uninstall.sh
+++ install/uninstall.sh
@@ -4,3 +4,4 @@ for i in artifacts/crd*yaml; do kubectl delete -f $i; done
 kubectl delete -f otdd-controller.yaml
 for i in artifacts/istio-mixer*.yaml; do kubectl delete -f $i; done
 kubectl delete -f otdd-adapter.yaml
+kubectl delete -f namespace.yaml
