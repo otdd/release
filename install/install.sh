b0d0833c11095237d5e66c131d9d5efb7b515b18
yipjie <dev@otdd.io>
2020-01-17 16:45:57 +0800

    update

--- install/install.sh
+++ install/install.sh
@@ -3,6 +3,7 @@ kubectl -n istio-system get configmap istio-sidecar-injector -o yaml | sed "s/is
 kubectl -n istio-system rollout restart deploy/istio-sidecar-injector
 #install otdd recorder crd and it's k8s controller
 for i in artifacts/crd*yaml; do kubectl apply -f $i; done
-kubectl apply -f otdd.yaml
+kubectl apply -f otdd-controller.yaml
 #install istio mixer otdd adapter
 for i in artifacts/istio-mixer*.yaml; do kubectl apply -f $i; done
+kubectl apply -f otdd-adapter.yaml
--- install/uninstall.sh
+++ install/uninstall.sh
@@ -1,4 +1,6 @@
 kubectl -n istio-system get configmap istio-sidecar-injector -o yaml | sed "s/otdd\/proxyv2:1.2.2-alpha.0/istio\/proxyv2:1.2.2/g"| kubectl apply -f -
 kubectl -n istio-system rollout restart deploy/istio-sidecar-injector
 for i in artifacts/crd*yaml; do kubectl delete -f $i; done
-kubectl delete -f otdd.yaml
+kubectl delete -f otdd-controller.yaml
+for i in artifacts/istio-mixer*.yaml; do kubectl delete -f $i; done
+kubectl delete -f otdd-adapter.yaml
