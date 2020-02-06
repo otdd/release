0385923ff3903f30240fe895d2f4ead571954a7e
yipjie <dev@otdd.io>
2020-02-06 22:50:04 +0800

    update

--- install/install.sh
+++ install/install.sh
@@ -20,8 +20,12 @@ then
   usage
 fi
 
-#change istio proxyv2 to include otdd redirect/recorder plugins.
-kubectl -n istio-system get configmap istio-sidecar-injector -o yaml | sed "s/istio\/proxyv2:$ISTIO_VERSION/otdd\/proxyv2:$ISTIO_VERSION-otdd.0.1.0/g"| kubectl apply -f -
+#replace istio proxyv2 image to otdd proxyv2 to include otdd redirect/recorder plugins by changing the .Values.global.proxy.image in the configmap.
+#write to a temp file then VALUE=`cat .otdd_tmp instead of using VALUE=`...` directly because the \n will lost wiredly. 
+kubectl -n istio-system get configmap istio-sidecar-injector -o json | jq ".data.config" |awk -v version="$ISTIO_VERSION" '{gsub(/name:[ ]+istio-proxy.+[ ]{4}ports/,"name: istio-proxy\\n    image: docker.io/otdd/proxyv2:" version "-otdd.0.1.0\\n    ports"); print $0}' > .otdd_tmp
+VALUE=`cat .otdd_tmp`
+rm -rf .otdd_tmp
+kubectl -n istio-system get configmap istio-sidecar-injector -o json | jq ".data.config=$VALUE" | kubectl apply -f -
 kubectl -n istio-system rollout restart deploy/istio-sidecar-injector
 
 #create otdd-system namespace
@@ -33,3 +37,5 @@ kubectl apply -f artifacts/otdd-controller.yaml
 #install istio mixer otdd adapter
 for i in artifacts/istio-mixer*.yaml; do kubectl apply -f $i; done
 kubectl apply -f artifacts/otdd-adapter.yaml
+#install the otdd-server
+kubectl apply -f artifacts/otdd-server.yaml
--- install/uninstall.sh
+++ install/uninstall.sh
@@ -1,27 +1,11 @@
-usage(){
-        echo "Usage: uninstall.sh -v istio-version"
-        echo ""
-        echo -e "\texamples: \t\tunstall.sh -v 1.2.2"
-        exit 1
-}
-
-while getopts v: option
-do
- case "${option}"
- in
- v) ISTIO_VERSION=${OPTARG};;
- esac
-done
-
-if [ -z "$ISTIO_VERSION" ]
-then
-  usage
-fi
-
-kubectl -n istio-system get configmap istio-sidecar-injector -o yaml | sed "s/otdd\/proxyv2:$ISTIO_VERSION-otdd.0.1.0/istio\/proxyv2:$ISTIO_VERSION/g"| kubectl apply -f -
+kubectl -n istio-system get configmap istio-sidecar-injector -o json | jq ".data.config" |awk '{gsub(/name:[ ]+istio-proxy.+[ ]{4}ports/,"name: istio-proxy\\n  {{- if contains \\\"/\\\" .Values.global.proxy.image }}\\n    image: \\\"{{ annotation .ObjectMeta `sidecar.istio.io/proxyImage` .Values.global.proxy.image }}\\\"\\n  {{- else }}\\n    image: \\\"{{ annotation .ObjectMeta `sidecar.istio.io/proxyImage` .Values.global.hub }}/{{ .Values.global.proxy.image }}:{{ .Values.global.tag }}\\\"\\n  {{- end }}\\n    ports"); print $0}' > .otdd_tmp_uninstall
+VALUE=`cat .otdd_tmp_uninstall`
+rm -rf .otdd_tmp_uninstall
+kubectl -n istio-system get configmap istio-sidecar-injector -o json | jq ".data.config=$VALUE" | kubectl apply -f -
 kubectl -n istio-system rollout restart deploy/istio-sidecar-injector
 for i in artifacts/crd*yaml; do kubectl delete -f $i; done
 kubectl delete -f artifacts/otdd-controller.yaml
 for i in artifacts/istio-mixer*.yaml; do kubectl delete -f $i; done
 kubectl delete -f artifacts/otdd-adapter.yaml
+kubectl delete -f artifacts/otdd-server.yaml
 kubectl delete -f artifacts/namespace.yaml
